b1526b1bfd45e46b3d3a2f5872eff4e627d35bde
chris <taiwoakinfenwa2019@gmail.com>
2024-08-13 14:42:38 +0100

    ..

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -26,22 +26,22 @@ onMounted(() => {
             </div>
         </div>
        <div class="container  text-center">
-        <div class="d-lg-flex team gap-3">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/Pastor5.jpg" alt="Card image cap"  height="250px" style="object-fit: cover; object-position: center;" >
+        <div class="d-lg-flex team gap-3 justify-content-center align-content-center ">
+            <div class="card byn">
+                <img class="card-img-top" src="../assets/images/Pastor5.jpg" alt="Card image cap"  height="250px" >
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                 <button class="w-100">Read More</button>
               </div>
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/Pastor2.jpg" alt="Card image cap" height="250px" style="object-fit: cover; ">
+              <div class="card byn">
+                <img class="card-img-top" src="../assets/images/Pastor2.jpg" alt="Card image cap" height="250px" >
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                 <button class="w-100">Read More</button>
               </div>
-              <div class="card" style="width: 18rem;">
+              <div class="card byn">
                 <picture>
                     <img class="w-100 object-fit-cover" src="../assets/images/p1.jpg" alt="Card image cap"  height="250px" >
                 </picture>
@@ -50,9 +50,9 @@ onMounted(() => {
                 </div>
                 <button class="w-100">Read More</button>
               </div>
-              <div  class="card" style="width: 18rem;">
+              <div  class="card byn">
                 <div class="profil">
-                 <img class="card-img-top" src="../assets/images/p9.jpeg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+                 <img class="card-img-top" src="../assets/images/p9.jpeg" alt="Card image cap"  height="250px">
                  <div class="card-body">
                      <h6 class="name">Pastor Ademola Moses Olubode [Director of Exams, Records, and Policy Issues]</h6>
                      <p class="bio">Pastor Olubode is the Parish Pastor of RCCG International Chapel, Long Island (RCCGHHH). He is also a AWS/GRC and Cybersecurity Analyst. He enjoys playing music and sports.</p>
@@ -62,8 +62,8 @@ onMounted(() => {
            </div>
         </div>
         <div data-aos="fade-right" class=" d-lg-flex team gap-3 mt-5">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/p2.jpeg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+            <div class="card byn">
+                <img class="card-img-top" src="../assets/images/p2.jpeg" alt="Card image cap"  height="250px" >
                 <div class="card-body">
                     <h6 class="name">Pastor Michael Oluwafemi [National coordinator RCCG Americas Continent 1]</h6>
                     <p class="bio">Pastor Oluwafemi is one of the Special Assistant to the Continental Overseer (SATCO) and the pastor in
@@ -73,9 +73,9 @@ onMounted(() => {
                 <button class="w-100">Read More</button>
 
               </div>
-              <div  class="card" style="width: 18rem;">
+              <div  class="card byn">
                 <div class="profil">
-                 <img class="card-img-top" src="../assets/images/p8.jpg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+                 <img class="card-img-top" src="../assets/images/p8.jpg" alt="Card image cap"  height="250px" >
                  <div class="card-body">
                     <h6 class="name">Pastor Enoch Badamosi [Assistant Director of Exams, Records, and Policy Issues]</h6>
                     <p class="bio">Pastor Enoch Badamosi is the Pastor in charge of PA Zone 2, The Americas. He is a senior health program Analyst with the City of Philadelphia.trained attorney with LL.M Health Law & LL.M Corporate
@@ -86,16 +86,16 @@ onMounted(() => {
              </div>
            </div>
              
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/p4.jpg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+              <div class="card byn" >
+                <img class="card-img-top" src="../assets/images/p4.jpg" alt="Card image cap"  height="250px">
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
                 <button class="w-100">Read More</button>
 
               </div>
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/p5.jpg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+              <div class="card byn" >
+                <img class="card-img-top" src="../assets/images/p5.jpg" alt="Card image cap"  height="250px" >
                 <div class="card-body">
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
@@ -104,8 +104,8 @@ onMounted(() => {
               </div>
           </div>
           <div data-aos="fade-right" class="d-lg-flex team gap-3 mt-5">
-              <div  class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/p6.jpg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+              <div  class="card byn">
+                <img class="card-img-top" src="../assets/images/p6.jpg" alt="Card image cap"  height="250px">
                      <div class="card-body">
                          <h6 class="name">Dr. Funto Oyewole [Director of Publications]</h6>
                          <p class="bio">Dr. Oyewole is a clinical psychologist and an ordained minister. She is passionate about
@@ -116,16 +116,16 @@ onMounted(() => {
 
      
                  </div>
-                 <div  class="card" style="width: 18rem;">
-                     <img class="card-img-top" src="../assets/images/p7.jpg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+                 <div  class="card byn">
+                     <img class="card-img-top" src="../assets/images/p7.jpg" alt="Card image cap"  height="250px">
                      <div class="card-body">
                          <h6 class="name">Pastor Ademola Moses Olubode [Director of Exams, Records, and Policy Issues]</h6>
                          <p class="bio">Pastor Olubode is the Parish Pastor of RCCG International Chapel, Long Island (RCCGHHH). He is also a AWS/GRC and Cybersecurity Analyst. He enjoys playing music and sports.</p>
                      </div> 
                      <button class="w-100">Read More</button>
                </div>
-               <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="../assets/images/p3.jpg" alt="Card image cap"  height="250px" style="object-fit: cover;">
+               <div class="card byn">
+                <img class="card-img-top" src="../assets/images/p3.jpg" alt="Card image cap"  height="250px" >
                 <div class="card-body">
                     <h6 class="name">Pastor Mike Daniel [Director of Events and Programs]</h6>
                     <p class="bio">Pastor Mike Daniel joined the Redeemed Christian Church of God in 1989. He completed the
@@ -145,6 +145,12 @@ onMounted(() => {
 .bio{
     font-size: 14px;
 }
+.byn{
+    width: 18rem;
+}
+img{
+    object-fit: cover;
+}
 .home{
     background: rgb(30, 18, 70);
 }
@@ -159,6 +165,14 @@ onMounted(() => {
     background-size: cover;
     background-position: center;
 }
-
+@media (max-width:720px){
+.byn{
+    width: 100%;
+    margin-bottom: 10px;
+}
+img{
+    object-fit: cover;
+}
+}
 
 </style>
