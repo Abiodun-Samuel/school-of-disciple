24a62096fcfba8c23cf8b8572816b830be03a3ee
chris <taiwoakinfenwa2019@gmail.com>
2024-09-02 15:10:18 +0100

    new adjustment on abouthomepage

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,96 +1,211 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div class="d-flex flex-column justify-content-center align-items-center mt-4 gap-3">
-        <div class="d-flex flex-md-row flex-column gap-3 container ">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3 ">
-                    <picture>
-                        <img src="../assets/images/Pastor5.jpg" class="img-fluid object-fit-cover  h-100 " width="900" alt="...">
-                      </picture>
-                    <p class="flex-1 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
+      <div class="d-flex flex-md-row flex-column gap-3 container">
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/Pastor5.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+              <p class="head">Pastor E.A. Adeboye</p>
+              <p class="subhead">Principal/Visioner</p>
+              <p class="details text-justify d-flex">
+                The General Overseer of The Redeemed Christian Church of God is Pastor Enoch Adejare Adeboye, known as “Daddy G.O.”
+                In 1981, Pastor E.A. Adeboye became the General Overseer (the spiritual and administrative head of the RCCG) since his appointment the church has since experienced unprecedented growth
+              </p>
             </div>
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3">
-                    <picture>
-                        <img src="../assets/images/Pastor2.jpg" class="img-fluid object-fit-cover  h-100 " width="900" alt="...">
-                      </picture>
-                    <img  alt="" height="100" class="mx-auto d-block"/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
+          </div>
         </div>
-        <div class="d-flex flex-md-row flex-column gap-3 container ">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3 ">
-                    <picture>
-                        <img src="../assets/images/p7.jpg" class="img-fluid object-fit-cover  h-100 " width="900" alt="...">
-                      </picture>
-                    <p class="flex-1 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
+  
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/Pastor2.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+              <p class="head">Pastor (Mrs) Folu Adeboye</p>
+              <p class="subhead">Vice President (Education)</p>
+              <p class="details">
+                Pastor (Mrs) Folu Adeboye is the wife of the General Overseer and has a heart of pursuing the good of ordinary people. She is a veteran teacher and started her teaching career as a spinster and in the course of her life acquired various teaching skills.
+
+                Her seasoned experience as a teacher qualified her to be appointed as the Headmistress of Christ the Redeemers’ Nursery and Primary School, Ebute-Metta, Lagos.              </p>
             </div>
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3">
-                    <picture>
-                        <img src="../assets/images/p8.jpg" class="img-fluid object-fit-cover  h-100 " width="900" alt="...">
-                      </picture>
-                    <img  alt="" height="100" class="mx-auto d-block"/>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
+          </div>
         </div>
-        <div class="d-flex flex-md-row flex-column gap-3 container ">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3 ">
-                    <picture>
-                        <img src="../assets/images/p6.jpg" class="img-fluid object-fit-cover h-100 " width="900" alt="...">
-                      </picture>
-                    <p class="flex-1 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
+      </div>
+  
+      <div class="d-flex flex-md-row flex-column gap-3 container">
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-2 ">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p8.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+              <p class="head">Pastor Enoch Badamosi</p>
+              <p class="subhead">Assistant Director of Exams, Records, and Policy Issues</p>
+              <p class="details">Pastor Enoch Badamosi is the Pastor in charge of PA Zone 2, The Americas 1. He is a senior health program Analyst with the City of Philadelphia. He is a trained attorney with LL.M Health Law & LL.M Corporate Law & Finance from Widener Law School, DE. He is a doctoral candidate in Public Policy & Administration at Walden University, MN and a 
+                doctoral candidate in Transformational Leadership @ Redeemer’s University of North America
+                </p>
             </div>
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3">
-                    <picture>
-                        <img src="../assets/images/p1.jpg" class="img-fluid  h-100 object-fit-cover" width="900" alt="...">
-                      </picture>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
+          </div>
         </div>
-        <div class="d-flex flex-md-row flex-column gap-3 container ">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3 ">
-                    <picture>
-                        <img src="../assets/images/p2.jpeg" class="img-fluid object-fit-cover  h-100 " width="900" alt="...">
-                      </picture>
-                    <p class="flex-1 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3">
-                    <picture>
-                        <img src="../assets/images/p3.jpg" class="img-fluid object-fit-cover  h-100 " width="900" alt="...">
-                      </picture>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
+  
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p7.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Pastor Ademola Moses Olubode</p>
+                <p class="subhead">Director of Exams, Records, and Policy Issues</p>
+                <p class="details">Pastor Olubode is the Parish Pastor of RCCG International Chapel, Long Island (RCCGHHH). 
+                    He is also a AWS/GRC and Cybersecurity Analyst. 
+                    He enjoys playing music and sports. 
+                  </p>
+              </div>
+          </div>
         </div>
-        <div class="d-flex flex-md-row flex-column gap-3 container ">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3">
-                    <picture>
-                        <img src="../assets/images/p4.jpg" class="img-fluid object-fit-cover h-100 " width="1000" alt="...">
-                      </picture>
-                    <p class="flex-1 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body d-flex gap-3">
-                    <picture>
-                        <img src="../assets/images/p5.jpg" class="img-fluid object-fit-cover  h-100 " width="1000" alt="...">
-                      </picture>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorem quisquam suscipit architecto, culpa sapiente autem illo placeat perferendis aut doloremque rem cumque. Necessitatibus, quae natus autem perferendis ratione iure.</p>
-                </div>
-            </div>
+      </div>
+  
+      <div class="d-flex flex-md-row flex-column gap-3 container">
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p6.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Dr. Funto Oyewole</p>
+                <p class="subhead">Director of Publications</p>
+                <p class="details">Dr. Oyewole is a clinical psychologist and an ordained minister. She is passionate about Christians living in good mental health. She is an educator, public speaker, and writer.  
+                  </p>
+              </div>
+          </div>
         </div>
+  
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p1.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Pastor Juwon Ariyo</p>
+                <p class="subhead">Assistant Coordinator</p>
+                <p class="details">Juwon Ariyo a Pastor and a Teacher. He had his training at various levels of Degrees/Certifications in IT related fields and Management Sciences.
+
+                    A Human Resource Professional and a Researcher. 
+                  </p>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-md-row flex-column gap-3 container">
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p9.jpeg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Pastor Emmanuel Ojuolape</p>
+                <p class="subhead">Assistant Director of Exams, Records, and Policy Issues</p>
+                <p class="details">Pastor Emmanuel O. Ojuolape is the Lead Pastor of RCCG Word of Life Center (WOLC), Silver Spring, Maryland.
+ 
+                    Pastor Emmanuel, a scholar of faith, holds a Master’s degree in Theology and is a distinguished graduate of the School of Disciples and School of Deliverance from Christ the Redeemer’s Ministry.
+                     
+                    He is happily married to his beloved wife, Elizabeth Ojuolape, and they are blessed with children.
+                    
+                  </p>
+              </div>
+          </div>
+        </div>
+  
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p3.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Pastor Mike Daniel</p>
+                <p class="subhead">Director of Events and Programs</p>
+                <p class="details">Pastor Mike Daniel joined the Redeemed Christian Church of God in 1989.
+                     He completed the School of Disciples and graduated in 1999. In 2003, he started the School
+                      of Disciples in Dallas, TX, USA. Presently, he serves as the Director of Programs at SOD.
+                  </p>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-md-row flex-column gap-3 container">
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p4.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Pastor Segun Emmanuel</p>
+                <p class="subhead">Director of Events and Programs</p>
+                <p class="details">Pastor Mike Daniel joined the Redeemed Christian Church of God in 1989.
+                     He completed the School of Disciples and graduated in 1999. In 2003, he started the School
+                      of Disciples in Dallas, TX, USA. Presently, he serves as the Director of Programs at SOD.
+                  </p>
+              </div>
+          </div>
+        </div>
+  
+        <div class="card card-custom shadow p-3 mb-5 bg-white rounded">
+          <div class="card-body d-flex gap-3">
+            <picture class="flex-shrink-0" style="flex-basis: 40%;">
+              <img src="../assets/images/p5.jpg" class="img-fluid object-fit-cover h-100 w-100" alt="...">
+            </picture>
+            <div class="flex-grow-1" style="flex-basis: 60%; scrollbar-width: none; scroll-behavior: smooth;">
+                <p class="head">Pastor AIS Adeleye </p>
+                <p class="subhead">National Coordinator</p>
+                <p class="details">Pastor Samuel Idowu Adeleye is the National Coordinator of the School of Disciples. He was the Assistant 
+                    Pastor-in-Charge of Province (Admin,) Ekiti Province 3, Ekiti State before assuming this present office. Pastor Adeleye attended 
+                    St. Peter’s Primary School, Isona-Ilesa from 1972 – 1978.
+
+                    He attended CAC High School, Iyemogun Road, Ilesa and St. John Grammar School, Fugar, Auchi, Old Bendel 
+                    State for his secondary school education and finished in 1980. He graduated with B.Sc. Honours in Geography from Adeyemi
+                     College of Education, 
+                    Ilesa in 1994 and got a Masters Degree in Guidance and Counseling from University of Ibadan in 2000.
+                  </p>
+              </div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <style scoped>
+  .card-custom {
+    width: 100%;
+    height: 300px; /* Set a fixed height for the cards */
+  }
+  .details{
+    font-size: 13px;
+    
+  }
+  .head{
+    font-size: 16px;
+    font-weight: bolder;
+    color: #662d91;
+  }
+  
+  .card-body {
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+    height: 100%;
+  }
+  
+  .flex-shrink-0 img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+  
+  .flex-grow-1 {
+    overflow-y: auto; /* Prevent overflow from the text area */
+  }
+  </style>
+  
