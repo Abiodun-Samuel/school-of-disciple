bfa7315c323462602a1d4a0d8d23ac10ae932af3
chris <taiwoakinfenwa2019@gmail.com>
2024-09-09 18:45:21 +0100

    update landingpage

--- src/components/GetIntouch.vue
+++ src/components/GetIntouch.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="intouch">
+    <div class="intouch d-flex flex-column flex-md-row justify-content-between align-items-center">
         <div>
-            <h1>School of Disciples Learning Community</h1>
-            <h4 class="w-75 lh-base">Join this community to understand more about the School of Disciples courses, Enroll now</h4>
+            <h1 class="text-center text-md-start">School of Disciples Learning Community</h1>
+            <h4 class="col-md-9 col-12 text-center text-md-start lh-base">Join this community to understand more about the School of Disciples courses, Enroll now</h4>
         </div>
         <div>
             <button class="startbtn">Get Started</button>
@@ -12,12 +12,9 @@
 <style>
 .intouch{
     background-image:linear-gradient(180deg, #0bccee 0%, rgba(0, 0, 0, 0.51) 0%, rgba(0, 0, 0, 0.5) 0%,
-    rgba(255, 255, 255, 0) 100%), url(../assets/images/sod5.jpeg);  
+    rgba(255, 255, 255, 0) 100%), url(../assets/images/sod3.jpeg);  
     height: 300px;
     color: white;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     padding: 0 80px;
     background-size: cover;
 }
@@ -25,6 +22,15 @@
     background: linear-gradient(to right, rgb(15, 197, 239) 0%, rgb(141, 216, 22) 100%);
     width: 200px;
     font-size: 20px;
+}
+.startbtn:hover{
+    background: linear-gradient(to right,rgb(141, 216, 22)  0%,  rgb(15, 197, 239) 100%);
 
+}
+@media screen and (max-width: 520px) {
+    .intouch{
+        padding:10px;
+    }
+    
 }
 </style>
